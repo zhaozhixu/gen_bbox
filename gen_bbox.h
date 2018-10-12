505ae43d66037286e1d088844eba397c805efbba
Zhao Zhixu <zhixu001@gmail.com>
2018-10-12 09:42:25 +0800

    make c++ compitable

--- gen_bbox.h
+++ gen_bbox.h
@@ -3,9 +3,19 @@
 
 struct pre_alloc_tensors;
 
+#ifdef __cplusplus
+#define GB_CPPSTART extern "C" {
+#define GB_CPPEND }
+GB_CPPSTART
+#endif
+
 struct pre_alloc_tensors *gb_preprocess(void);
 void gb_getbbox(int16_t *feature, int img_width, int img_height,
                 struct pre_alloc_tensors *tensors, float *result);
 void gb_postprocess(struct pre_alloc_tensors *tensors);
+
+#ifdef __cplusplus
+GB_CPPEND
+#endif
 
 #endif  /* _GEN_BBOX_H_ */
